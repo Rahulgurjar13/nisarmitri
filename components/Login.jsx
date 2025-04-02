65adce8a379a2fdb6ab56c8c50be622449a52079
Rahul Gurjar <rahulgurjar@Rahuls-MacBook-Air.local>
2025-04-02 11:09:47 +0530

    new

--- components/Login.jsx
+++ components/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5173/api/auth/login",
+        "https://nisarmitri.vercel.app/api/auth/login",
         {
           email,
           password,
